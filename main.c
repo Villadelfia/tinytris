99931fa322a8d28527f4578f13ca18ef17e9ede7
Pandora Nova <randy.thiemann@gmail.com>
2025-05-08 15:42:30 +0200

    More progress.

--- main.c
+++ main.c
@@ -3,30 +3,133 @@
 #include <SDL3/SDL_main.h>
 #include <SDL3_image/SDL_image.h>
 #include <SDL3/SDL_timer.h>
+#include <stdint.h>
+#include <time.h>
 
+// Offset of the field from the top left of the window.
+const float FIELD_X_OFFSET = 16.0f;
+const float FIELD_Y_OFFSET = 16.0f * 4.0f;
+
+// Render scale.
+const int RENDER_SCALE = 2;
+
+// Renderer and target.
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 static SDL_Texture *blockTexture = NULL;
 static Uint64 lastTime = 0;
+static Uint64 frameCtr = 0;
 
-const float FIELD_X_OFFSET = 16.0f;
-const float FIELD_Y_OFFSET = 16.0f * 4.0f;
-
-SDL_HitTestResult HitTest(SDL_Window *win, const SDL_Point *area, void *data) {
-    return SDL_HITTEST_DRAGGABLE;
-}
-
+// Types for game state.
 typedef enum {BLOCK_VOID, BLOCK_X, BLOCK_I, BLOCK_L, BLOCK_O, BLOCK_Z, BLOCK_T, BLOCK_J, BLOCK_S} block_type_t;
 typedef enum {LOCK_LOCKED, LOCK_FLASH, LOCK_UNLOCKED, LOCK_GHOST} lock_status_t;
 typedef struct {
     block_type_t type;
-    lock_status_t lockStatus;
-    float lockParam;
+    lock_status_t lock_status;
+    float lock_param;
 } block_t;
+typedef struct {
+    block_type_t type;
+    uint8_t rotation_state;
+    uint8_t x;
+    uint8_t y;
+    lock_status_t lock_status;
+    float lock_param;
+} live_block_t;
 
-block_t field[10][20] = {0};
+// The current field of locked pieces.
+block_t field[10][21] = {0};
+block_type_t history[4] = {0};
+block_type_t next_piece;
+live_block_t current_piece;
 
-void renderBlockImpl(int col, int row, block_type_t block, lock_status_t lockStatus, float lockParam, bool voidToLeft, bool voidToRight, bool voidAbove, bool voidBelow) {
+// SDL hit-test that marks the entire window as draggable.
+SDL_HitTestResult HitTest(SDL_Window *win, const SDL_Point *area, void *data) {
+    return SDL_HITTEST_DRAGGABLE;
+}
+
+// Splitmix64 generator for seeding.
+uint64_t splitmix_state;
+static uint64_t xoroshiro_state[4];
+uint64_t splitmix_next() {
+    splitmix_state += 0x9e3779b97f4a7c15ULL;
+    uint64_t z = splitmix_state;
+    z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9ULL;
+    z = (z ^ (z >> 27)) * 0x94d049bb133111ebULL;
+    return z ^ (z >> 31);
+}
+
+static uint64_t rotl(const uint64_t x, int k) {
+    return (x << k) | (x >> (64 - k));
+}
+
+uint64_t xoroshiro_next() {
+    const uint64_t result = rotl(xoroshiro_state[1] * 5, 7) * 9;
+    const uint64_t t = xoroshiro_state[1] << 17;
+    xoroshiro_state[2] ^= xoroshiro_state[0];
+    xoroshiro_state[3] ^= xoroshiro_state[1];
+    xoroshiro_state[1] ^= xoroshiro_state[2];
+    xoroshiro_state[0] ^= xoroshiro_state[3];
+    xoroshiro_state[2] ^= t;
+    xoroshiro_state[3] = rotl(xoroshiro_state[3], 45);
+    return result;
+}
+
+void seed_rng() {
+    splitmix_state = time(NULL);
+    xoroshiro_state[0] = splitmix_next();
+    xoroshiro_state[1] = splitmix_next();
+    xoroshiro_state[2] = splitmix_next();
+    xoroshiro_state[3] = splitmix_next();
+}
+
+block_type_t generate_piece() {
+    uint8_t piece = xoroshiro_next() % 8;
+    while(piece > 6) piece = xoroshiro_next() % 8;
+    return (block_type_t)(piece + 2);
+}
+
+void generate_first_piece() {
+    history[1] = BLOCK_Z;
+    history[2] = BLOCK_S;
+    history[3] = BLOCK_S;
+    block_type_t result = generate_piece();
+    while(result == BLOCK_S || result == BLOCK_Z || result == BLOCK_O) result = generate_piece();
+    history[0] = result;
+    current_piece = (live_block_t){0};
+    current_piece.type = BLOCK_VOID;
+    next_piece = result;
+}
+
+void generate_next_piece() {
+    block_type_t result = generate_piece();
+    int tries = 1;
+    while(tries++ < 6) {
+        if(result != history[0] && result != history[1] && result != history[2] && result != history[3]) {
+            break;
+        } else {
+            result = generate_piece();
+        }
+    }
+    history[0] = history[1];
+    history[1] = history[2];
+    history[2] = history[3];
+    history[3] = result;
+
+    current_piece = (live_block_t){0};
+    current_piece.type = next_piece;
+    next_piece = result;
+    current_piece.x = 3;
+    current_piece.y = 0;
+    current_piece.lock_param = 1.0f;
+    current_piece.lock_status = LOCK_UNLOCKED;
+    current_piece.rotation_state = 0;
+
+    // TODO: Apply 20G if needed.
+    // TODO: Apply IRS.
+}
+
+void render_raw_block(int col, int row, block_type_t block, lock_status_t lockStatus, float lockParam, bool voidToLeft, bool voidToRight, bool voidAbove, bool voidBelow) {
     SDL_FRect src = {0};
     SDL_FRect dest = {
         .x = FIELD_X_OFFSET + ((float)col * 16.0f),
@@ -107,64 +210,130 @@ void renderBlockImpl(int col, int row, block_type_t block, lock_status_t lockSta
     SDL_SetRenderDrawColor(renderer, r, g, b, a);
 }
 
-void renderBlock(int x, int y) {
-    if(x < 0 || x > 9 || y < 0 || y > 19) return;
+void render_field_block(int x, int y) {
+    if(x < 0 || x > 9 || y < 1 || y > 20) return;
     bool voidToLeft = x != 0 && field[x-1][y].type == BLOCK_VOID;
     bool voidToRight = x != 9 && field[x+1][y].type == BLOCK_VOID;
-    bool voidAbove = y != 0 && field[x][y-1].type == BLOCK_VOID;
-    bool voidBelow = y != 19 && field[x][y+1].type == BLOCK_VOID;
-    renderBlockImpl(x, y, field[x][y].type, field[x][y].lockStatus, field[x][y].lockParam, voidToLeft, voidToRight, voidAbove, voidBelow);
+    bool voidAbove = y != 1 && field[x][y-1].type == BLOCK_VOID;
+    bool voidBelow = y != 20 && field[x][y+1].type == BLOCK_VOID;
+    render_raw_block(x, y-1, field[x][y].type, field[x][y].lock_status, field[x][y].lock_param, voidToLeft, voidToRight, voidAbove, voidBelow);
+}
+
+void render_next_block() {
+    switch(next_piece) {
+        case BLOCK_I:
+            render_raw_block(3, -3, BLOCK_I, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            render_raw_block(4, -3, BLOCK_I, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            render_raw_block(5, -3, BLOCK_I, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            render_raw_block(6, -3, BLOCK_I, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            break;
+        case BLOCK_L:
+            render_raw_block(3, -3, BLOCK_L, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            render_raw_block(4, -3, BLOCK_L, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            render_raw_block(5, -3, BLOCK_L, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            render_raw_block(3, -2, BLOCK_L, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            break;
+        case BLOCK_O:
+            render_raw_block(4, -3, BLOCK_O, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            render_raw_block(5, -3, BLOCK_O, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            render_raw_block(4, -2, BLOCK_O, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            render_raw_block(5, -2, BLOCK_O, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            break;
+        case BLOCK_Z:
+            render_raw_block(3, -3, BLOCK_Z, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            render_raw_block(4, -3, BLOCK_Z, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            render_raw_block(4, -2, BLOCK_Z, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            render_raw_block(5, -2, BLOCK_Z, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            break;
+        case BLOCK_T:
+            render_raw_block(3, -3, BLOCK_T, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            render_raw_block(4, -3, BLOCK_T, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            render_raw_block(5, -3, BLOCK_T, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            render_raw_block(4, -2, BLOCK_T, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            break;
+        case BLOCK_J:
+            render_raw_block(3, -3, BLOCK_J, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            render_raw_block(4, -3, BLOCK_J, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            render_raw_block(5, -3, BLOCK_J, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            render_raw_block(5, -2, BLOCK_J, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            break;
+        case BLOCK_S:
+            render_raw_block(4, -3, BLOCK_S, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            render_raw_block(5, -3, BLOCK_S, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            render_raw_block(3, -2, BLOCK_S, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            render_raw_block(4, -2, BLOCK_S, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+            break;
+        default:
+            return;
+    }
+}
+
+void render_current_block() {
+    if(current_piece.type == BLOCK_VOID) return;
+    // TODO.
+}
+
+void render_field() {
+    for (int x = 0; x < 10; x++) {
+        for (int y = 1; y < 21; y++) {
+            render_field_block(x, y);
+        }
+    }
 }
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
+    seed_rng();
+    generate_first_piece();
+
     // Create the window.
     SDL_SetAppMetadata("Tinytris", "1.0", "org.villadelfia.tinytris");
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_CreateWindowAndRenderer("Tinytris", 12*16, 26*16, SDL_WINDOW_TRANSPARENT | SDL_WINDOW_ALWAYS_ON_TOP | SDL_WINDOW_BORDERLESS, &window, &renderer);
+    SDL_CreateWindowAndRenderer("Tinytris", 12*16*RENDER_SCALE, 26*16*RENDER_SCALE, SDL_WINDOW_TRANSPARENT | SDL_WINDOW_ALWAYS_ON_TOP | SDL_WINDOW_BORDERLESS, &window, &renderer);
+    SDL_SetRenderScale(renderer, 1.0f * (float)RENDER_SCALE, 1.0f * (float)RENDER_SCALE);
 
     // Make the window entirely draggable.
     SDL_SetWindowHitTest(window, HitTest, NULL);
 
     // Load the image for a block.
     blockTexture = IMG_LoadTexture(renderer, "block.bmp");
-    SDL_Log("%s", SDL_GetError());
+    SDL_SetTextureScaleMode(blockTexture, SDL_SCALEMODE_NEAREST);
 
     // Test data.
-    field[3][15] = (block_t){.type = BLOCK_T, .lockStatus = LOCK_LOCKED};
-    field[4][15] = (block_t){.type = BLOCK_L, .lockStatus = LOCK_LOCKED};
-    field[5][15] = (block_t){.type = BLOCK_L, .lockStatus = LOCK_LOCKED};
+    field[3][16] = (block_t){.type = BLOCK_T, .lock_status = LOCK_LOCKED};
+    field[4][16] = (block_t){.type = BLOCK_L, .lock_status = LOCK_LOCKED};
+    field[5][16] = (block_t){.type = BLOCK_L, .lock_status = LOCK_LOCKED};
 
-    field[1][16] = (block_t){.type = BLOCK_T, .lockStatus = LOCK_LOCKED};
-    field[2][16] = (block_t){.type = BLOCK_T, .lockStatus = LOCK_LOCKED};
-    field[3][16] = (block_t){.type = BLOCK_T, .lockStatus = LOCK_LOCKED};
-    field[4][16] = (block_t){.type = BLOCK_T, .lockStatus = LOCK_LOCKED};
-    field[5][16] = (block_t){.type = BLOCK_L, .lockStatus = LOCK_LOCKED};
+    field[1][17] = (block_t){.type = BLOCK_T, .lock_status = LOCK_LOCKED};
+    field[2][17] = (block_t){.type = BLOCK_T, .lock_status = LOCK_LOCKED};
+    field[3][17] = (block_t){.type = BLOCK_T, .lock_status = LOCK_LOCKED};
+    field[4][17] = (block_t){.type = BLOCK_T, .lock_status = LOCK_LOCKED};
+    field[5][17] = (block_t){.type = BLOCK_L, .lock_status = LOCK_LOCKED};
 
-    field[0][17] = (block_t){.type = BLOCK_T, .lockStatus = LOCK_LOCKED};
-    field[1][17] = (block_t){.type = BLOCK_T, .lockStatus = LOCK_LOCKED};
-    field[2][17] = (block_t){.type = BLOCK_T, .lockStatus = LOCK_LOCKED};
-    field[3][17] = (block_t){.type = BLOCK_O, .lockStatus = LOCK_LOCKED};
-    field[4][17] = (block_t){.type = BLOCK_O, .lockStatus = LOCK_LOCKED};
-    field[5][17] = (block_t){.type = BLOCK_L, .lockStatus = LOCK_LOCKED};
+    field[0][18] = (block_t){.type = BLOCK_T, .lock_status = LOCK_LOCKED};
+    field[1][18] = (block_t){.type = BLOCK_T, .lock_status = LOCK_LOCKED};
+    field[2][18] = (block_t){.type = BLOCK_T, .lock_status = LOCK_LOCKED};
+    field[3][18] = (block_t){.type = BLOCK_O, .lock_status = LOCK_LOCKED};
+    field[4][18] = (block_t){.type = BLOCK_O, .lock_status = LOCK_LOCKED};
+    field[5][18] = (block_t){.type = BLOCK_L, .lock_status = LOCK_LOCKED};
 
-    field[0][18] = (block_t){.type = BLOCK_L, .lockStatus = LOCK_LOCKED};
-    field[1][18] = (block_t){.type = BLOCK_L, .lockStatus = LOCK_LOCKED};
-    field[2][18] = (block_t){.type = BLOCK_L, .lockStatus = LOCK_LOCKED};
-    field[3][18] = (block_t){.type = BLOCK_O, .lockStatus = LOCK_LOCKED};
-    field[4][18] = (block_t){.type = BLOCK_O, .lockStatus = LOCK_LOCKED};
-    field[6][18] = (block_t){.type = BLOCK_J, .lockStatus = LOCK_LOCKED};
-    field[7][18] = (block_t){.type = BLOCK_J, .lockStatus = LOCK_LOCKED};
-    field[8][18] = (block_t){.type = BLOCK_T, .lockStatus = LOCK_LOCKED};
+    field[0][19] = (block_t){.type = BLOCK_L, .lock_status = LOCK_LOCKED};
+    field[1][19] = (block_t){.type = BLOCK_L, .lock_status = LOCK_LOCKED};
+    field[2][19] = (block_t){.type = BLOCK_L, .lock_status = LOCK_LOCKED};
+    field[3][19] = (block_t){.type = BLOCK_O, .lock_status = LOCK_LOCKED};
+    field[4][19] = (block_t){.type = BLOCK_O, .lock_status = LOCK_LOCKED};
+    field[6][19] = (block_t){.type = BLOCK_J, .lock_status = LOCK_LOCKED};
+    field[7][19] = (block_t){.type = BLOCK_J, .lock_status = LOCK_LOCKED};
+    field[8][19] = (block_t){.type = BLOCK_T, .lock_status = LOCK_LOCKED};
 
-    field[0][19] = (block_t){.type = BLOCK_J, .lockStatus = LOCK_LOCKED};
-    field[1][19] = (block_t){.type = BLOCK_O, .lockStatus = LOCK_LOCKED};
-    field[2][19] = (block_t){.type = BLOCK_O, .lockStatus = LOCK_LOCKED};
-    field[3][19] = (block_t){.type = BLOCK_J, .lockStatus = LOCK_LOCKED};
-    field[4][19] = (block_t){.type = BLOCK_S, .lockStatus = LOCK_LOCKED};
-    field[5][19] = (block_t){.type = BLOCK_S, .lockStatus = LOCK_LOCKED};
-    field[6][19] = (block_t){.type = BLOCK_Z, .lockStatus = LOCK_LOCKED};
-    field[7][19] = (block_t){.type = BLOCK_Z, .lockStatus = LOCK_LOCKED};
-    field[8][19] = (block_t){.type = BLOCK_T, .lockStatus = LOCK_LOCKED};
+    field[0][20] = (block_t){.type = BLOCK_J, .lock_status = LOCK_LOCKED};
+    field[1][20] = (block_t){.type = BLOCK_O, .lock_status = LOCK_LOCKED};
+    field[2][20] = (block_t){.type = BLOCK_O, .lock_status = LOCK_LOCKED};
+    field[3][20] = (block_t){.type = BLOCK_J, .lock_status = LOCK_LOCKED};
+    field[4][20] = (block_t){.type = BLOCK_S, .lock_status = LOCK_LOCKED};
+    field[5][20] = (block_t){.type = BLOCK_S, .lock_status = LOCK_LOCKED};
+    field[6][20] = (block_t){.type = BLOCK_Z, .lock_status = LOCK_LOCKED};
+    field[7][20] = (block_t){.type = BLOCK_Z, .lock_status = LOCK_LOCKED};
+    field[8][20] = (block_t){.type = BLOCK_T, .lock_status = LOCK_LOCKED};
 
     return SDL_APP_CONTINUE;
 }
@@ -183,8 +352,10 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
     Uint64 now = SDL_GetTicksNS();
     if(now >= lastTime + (SDL_NS_PER_SECOND / 60LL) && SDL_GetKeyboardFocus() == window) {
         lastTime = now;
+        frameCtr++;
 
         // Do all the logic.
+        if (frameCtr % 30 == 0) generate_next_piece();
     }
 
     // Render the game.
@@ -225,16 +396,10 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
     dst = (SDL_FRect){.x = FIELD_X_OFFSET + (2.0f * 16.0f), .y = FIELD_Y_OFFSET - (3.0f * 16.0f) - 4, .w = 6.0f * 16.0f, .h = 2.0f * 16.0f + 8};
     SDL_RenderFillRect(renderer, &dst);
 
-    for (int x = 0; x < 10; x++) {
-        for (int y = 0; y < 20; y++) {
-            renderBlock(x, y);
-        }
-    }
-
-    renderBlockImpl(3, -3, BLOCK_T, LOCK_UNLOCKED, 1.0f, false, false, false, false);
-    renderBlockImpl(4, -3, BLOCK_T, LOCK_UNLOCKED, 1.0f, false, false, false, false);
-    renderBlockImpl(5, -3, BLOCK_T, LOCK_UNLOCKED, 1.0f, false, false, false, false);
-    renderBlockImpl(4, -2, BLOCK_T, LOCK_UNLOCKED, 1.0f, false, false, false, false);
+    // Render the game.
+    render_field();
+    render_next_block();
+    render_current_block();
 
     SDL_RenderPresent(renderer);
 
